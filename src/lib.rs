bb08e1d80d38be843fafef0d70a4706c18c97a55
EruEri <ndiayeyves@gmail.com>
2023-08-04 00:20:06 +0200

    [Add License]

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+// /////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                            //
+//  Copyright (C) 2023 Yves Ndiaye                                                            //
+//                                                                                            //
+// This Source Code Form is subject to the terms of the Mozilla Public                        //
+// License, v. 2.0. If a copy of the MPL was not distributed with this                        //
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.                                  //
+//                                                                                            //
+// /////////////////////////////////////////////////////////////////////////////////////////////
+
 pub mod cue_cd_text;
 pub mod cue_duration;
 pub mod cue_file_format;
--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,13 @@
+// /////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                            //
+//  Copyright (C) 2023 Yves Ndiaye                                                            //
+//                                                                                            //
+// This Source Code Form is subject to the terms of the Mozilla Public                        //
+// License, v. 2.0. If a copy of the MPL was not distributed with this                        //
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.                                  //
+//                                                                                            //
+// /////////////////////////////////////////////////////////////////////////////////////////////
+
 pub(crate) fn cue_format_string_value(s: &str) -> String {
     match s.contains(' ') {
         true => format!("\"{}\"", s),
