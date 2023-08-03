e55679672b9ec7b3a3c6a062c7e67e98525670d5
EruEri <ndiayeyves@gmail.com>
2023-08-04 01:05:31 +0200

    [Remove cuetrackbuilder]

--- src/cue_track.rs
+++ src/cue_track.rs
@@ -28,10 +28,6 @@ pub struct CueTrack {
     indexes: BTreeMap<u32, CueDuration>,
 }
 
-pub struct CueTrackBuilder {
-    track: CueTrack,
-}
-
 #[derive(Debug, Clone, Copy)]
 pub enum ComputeKind {
     Set(CueDuration),
@@ -58,9 +54,9 @@ impl Ord for CueTrack {
     }
 }
 
-impl CueTrackBuilder {
+impl CueTrack {
     pub fn new(track_index: u32, mode: CueTrackMode) -> Self {
-        let track = CueTrack {
+        let track = Self {
             track: (track_index, mode),
             flags: BTreeSet::new(),
             pregap: None,
@@ -69,91 +65,87 @@ impl CueTrackBuilder {
             rems: BTreeMap::new(),
             indexes: BTreeMap::new(),
         };
-        Self { track: track }
+        track
     }
 
     pub fn add_index(&mut self, index: u32, duration: DurationFormat) -> &mut Self {
-        let _ = self.track.indexes.insert(index, duration.to_duration());
+        let _ = self.indexes.insert(index, duration.to_duration());
         self
     }
 
     pub fn add_flag(&mut self, flag: CueTrackFlag) -> &mut Self {
-        let _ = self.track.flags.insert(flag);
+        let _ = self.flags.insert(flag);
         self
     }
 
     pub fn add_pregap(&mut self, duration: DurationFormat) -> &mut Self {
-        let _ = self.track.pregap = Some(duration.to_duration());
+        let _ = self.pregap = Some(duration.to_duration());
         self
     }
 
     pub fn add_postgap(&mut self, duration: DurationFormat) -> &mut Self {
-        let _ = self.track.postgap = Some(duration.to_duration());
+        let _ = self.postgap = Some(duration.to_duration());
         self
     }
 
     pub fn add_arranger(&mut self, arranger: &str) -> &mut Self {
         let arranger = CueCdText::Arrager(arranger.to_owned());
-        let _ = self.track.cd_texts.insert(arranger);
+        let _ = self.cd_texts.insert(arranger);
         self
     }
 
     pub fn add_composer(&mut self, composer: &str) -> &mut Self {
         let composer = CueCdText::Composer(composer.to_string());
-        let _ = self.track.cd_texts.insert(composer);
+        let _ = self.cd_texts.insert(composer);
         self
     }
 
     pub fn add_disc_id(&mut self, composer: &str) -> &mut Self {
         let disc_id = CueCdText::DiscId(composer.to_owned());
-        let _ = self.track.cd_texts.insert(disc_id);
+        let _ = self.cd_texts.insert(disc_id);
         self
     }
 
     pub fn add_genre(&mut self, genre: &str) -> &mut Self {
         let genre = CueCdText::Genre(genre.to_string());
-        let _ = self.track.cd_texts.insert(genre);
+        let _ = self.cd_texts.insert(genre);
         self
     }
 
     pub fn add_iscr(&mut self, iscr: &str) -> &mut Self {
         let iscr = CueCdText::ISrc(iscr.to_string());
-        let _ = self.track.cd_texts.insert(iscr);
+        let _ = self.cd_texts.insert(iscr);
         self
     }
 
     pub fn add_message(&mut self, message: &str) -> &mut Self {
         let message = CueCdText::Message(message.to_string());
-        let _ = self.track.cd_texts.insert(message);
+        let _ = self.cd_texts.insert(message);
         self
     }
 
     pub fn add_performer(&mut self, performer: &str) -> &mut Self {
         let performer = CueCdText::Performer(performer.to_string());
-        let _ = self.track.cd_texts.insert(performer);
+        let _ = self.cd_texts.insert(performer);
         self
     }
 
     pub fn add_songwriter(&mut self, songwriter: &str) -> &mut Self {
         let songwriter = CueCdText::SongWriter(songwriter.to_string());
-        let _ = self.track.cd_texts.insert(songwriter);
+        let _ = self.cd_texts.insert(songwriter);
         self
     }
 
     pub fn add_title(&mut self, title: &str) -> &mut Self {
         let title = CueCdText::Title(title.to_owned());
-        let _ = self.track.cd_texts.insert(title);
+        let _ = self.cd_texts.insert(title);
         self
     }
 
     pub fn add_rem(&mut self, key: &str, value: &str) -> &mut Self {
         let key = key.to_ascii_uppercase();
-        let _ = self.track.rems.insert(key, value.to_owned());
+        let _ = self.rems.insert(key, value.to_owned());
         self
-    }
-
-    pub fn track(self) -> CueTrack {
-        self.track
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,121 @@
 //                                                                                            //
 // /////////////////////////////////////////////////////////////////////////////////////////////
 
-pub mod cue_cd_text;
-pub mod cue_duration;
-pub mod cue_file_format;
-pub mod cue_sheet;
-pub mod cue_track;
-pub mod cue_track_flag;
-pub mod cue_track_mode;
+pub(crate) mod cue_cd_text;
+pub(crate) mod cue_duration;
+pub(crate) mod cue_file_format;
+pub(crate) mod cue_sheet;
+pub(crate) mod cue_track;
+pub(crate) mod cue_track_flag;
+pub(crate) mod cue_track_mode;
 pub(crate) mod util;
+
+pub use crate::cue_cd_text::CueCdText;
+pub use crate::cue_duration::{CueDuration, DurationFormat};
+pub use crate::cue_file_format::CueFileFormat;
+pub use crate::cue_sheet::{CueSheet, CueSheetBuilder};
+pub use crate::cue_track::{ComputeKind, CueTrack};
+pub use crate::cue_track_flag::CueTrackFlag;
+pub use crate::cue_track_mode::CueTrackMode;
+
+#[cfg(test)]
+mod test {
+    use crate::CueTrack;
+
+
+    #[test]
+    fn test_iridescent_vision() {
+        let mut dizzy = CueTrack::new(1, crate::CueTrackMode::AUDIO);
+        let _ = dizzy
+        .add_performer("喜多村英梨")
+        .add_composer("Village wood")
+        .add_title("D!zzy...&")
+        .add_index(1, crate::DurationFormat::MinSecMil(3, 54, 992));
+
+        let mut synapse = CueTrack::new(2, crate::CueTrackMode::AUDIO);
+        let _ = synapse
+        .add_performer("喜多村英梨")
+        .add_composer("古宇田亘d")
+        .add_title("SynApsE")
+        .add_index(1, crate::DurationFormat::MinSecMil(3, 15, 827));
+
+        let mut egoism = CueTrack::new(3, crate::CueTrackMode::AUDIO);
+        let _ = egoism
+        .add_performer("喜多村英梨")
+        .add_composer("Village wood")
+        .add_title("ヱゴヰズム")
+        .add_index(1, crate::DurationFormat::MinSecMil(3, 22, 147));
+
+        let mut anger = CueTrack::new(4, crate::CueTrackMode::AUDIO);
+        let _ = anger
+        .add_performer("喜多村英梨")
+        .add_composer("Han")
+        .add_title("ANGER%")
+        .add_index(1, crate::DurationFormat::MinSecMil(5, 12, 449));
+
+        let mut kindan_kajitsu = CueTrack::new(5, crate::CueTrackMode::AUDIO);
+        let _ = kindan_kajitsu
+        .add_performer("喜多村英梨")
+        .add_composer("Han")
+        .add_title("禁断果実")
+        .add_index(1, crate::DurationFormat::MinSecMil(3, 12, 565));
+
+        let mut belief_in_oneself = CueTrack::new(6, crate::CueTrackMode::AUDIO);
+        let _ = belief_in_oneself
+        .add_performer("喜多村英梨")
+        .add_composer("Han")
+        .add_title("Belief in Oneself")
+        .add_index(1, crate::DurationFormat::MinSecMil(3, 29, 798));
+    
+        let mut shine_going_up = CueTrack::new(7, crate::CueTrackMode::AUDIO);
+        let _ = shine_going_up
+        .add_performer("喜多村英梨")
+        .add_composer("YutoMinami")
+        .add_title("SH!NE GO!NG UP")
+        .add_index(1, crate::DurationFormat::MinSecMil(4, 23, 883));
+
+        let mut holy_shit = CueTrack::new(8, crate::CueTrackMode::AUDIO);
+        let _ = holy_shit
+        .add_performer("喜多村英梨")
+        .add_composer("Village wood")
+        .add_title("HOLy×SH!T")
+        .add_index(1, crate::DurationFormat::MinSecMil(4, 29, 836));
+
+        let mut eternity = CueTrack::new(9, crate::CueTrackMode::AUDIO);
+        let _ = eternity
+        .add_performer("喜多村英梨")
+        .add_composer("Village wood")
+        .add_title("ETERNiTY")
+        .add_index(1, crate::DurationFormat::MinSecMil(4, 32, 006));
+
+        let mut nijiiro = CueTrack::new(10, crate::CueTrackMode::AUDIO);
+        let _ = nijiiro
+        .add_performer("喜多村英梨")
+        .add_composer("古宇田亘")
+        .add_title("虹色")
+        .add_index(1, crate::DurationFormat::MinSecMil(3, 59, 187));
+
+        let mut egoism_mix = CueTrack::new(11, crate::CueTrackMode::AUDIO);
+        let _ = egoism_mix
+        .add_performer("喜多村英梨")
+        .add_composer("Village wood")
+        .add_title("ヱゴヰズム (- Pf MiX -)")
+        .add_index(1, crate::DurationFormat::MinSecMil(3, 33, 684));
+
+        let mut kindan_kajitsu_mix = CueTrack::new(12, crate::CueTrackMode::AUDIO);
+        let _ = kindan_kajitsu_mix
+        .add_performer("喜多村英梨")
+        .add_composer("Han")
+        .add_title("禁断果実 (- Pf MiX -)")
+        .add_index(1, crate::DurationFormat::MinSecMil(3, 22, 723));
+
+        let mut shine_going_up_mix = CueTrack::new(13, crate::CueTrackMode::AUDIO);
+        let _ = shine_going_up_mix
+        .add_performer("喜多村英梨")
+        .add_composer("YutoMinami")
+        .add_title("SH!NE GO!NG UP (- Pf MiX -)")
+        .add_index(1, crate::DurationFormat::MinSecMil(4, 40, 965));
+
+    
+    }
+}
